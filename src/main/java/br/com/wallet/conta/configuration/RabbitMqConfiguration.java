88f4c7f51c5a4b14c77f1e21ce2538c2931aced4
Rafael Castro <rafaelmp@ciandt.com>
2022-01-13 15:31:27 -0300

    ajustes no json

--- src/main/java/br/com/wallet/conta/configuration/RabbitMqConfiguration.java
+++ src/main/java/br/com/wallet/conta/configuration/RabbitMqConfiguration.java
@@ -2,14 +2,11 @@ package br.com.wallet.conta.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -37,14 +34,6 @@ public class RabbitMqConfiguration {
     @Bean
     Binding binding(Queue queue, TopicExchange exchange) {
         return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
-    }
-
-    @Bean
-    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory) {
-        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory);
-        container.setQueueNames(QUEUE_NAME);
-        return container;
     }
 
     @Bean
--- src/main/java/br/com/wallet/conta/publisher/RabbitMqPublisher.java
+++ src/main/java/br/com/wallet/conta/publisher/RabbitMqPublisher.java
@@ -5,8 +5,11 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
@@ -23,7 +26,16 @@ public class RabbitMqPublisher {
     @Async
     public void enviarRabbitMQ(TransacaoEvent mensagem) throws JsonProcessingException {
         log.info("Enviando mensagem {}", mensagem);
-        this.template.convertAndSend(EXCHANGE_NAME, ROUTING_KEY, new ObjectMapper().writeValueAsString(mensagem));
+        final MessageProperties messageProperties = new MessageProperties();
+
+        this.sendMessage(mensagem, EXCHANGE_NAME, messageProperties, ROUTING_KEY);
+    }
+
+    private void sendMessage(final Object message, final String exchange, final MessageProperties messageProperties, final String routingKey) throws JsonProcessingException {
+        messageProperties.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        template.convertAndSend(exchange, routingKey != null ? routingKey : messageProperties.getReceivedRoutingKey(),
+                new Message(new ObjectMapper().writeValueAsBytes(message), messageProperties));
+
     }
 
 }
